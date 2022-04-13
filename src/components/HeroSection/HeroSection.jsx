d4ea8abcf84da489fd23bef7ee89c5c0f4612350
Muhammad Jan <m.janbugti12@gmail.com>
2022-04-13 17:58:16 +0500

    header styled

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from "styled-components";
+import BG from '../../assets/img/BG.png';
+import sergeyImg from '../../assets/img/sergey-vinogradov.png'
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+const Content = styled.div`
+ color: #fff;
+`
+
+const SocialIcons= styled.div`
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  :hover {
+    transform:scale(1.25, 1.25);
+  }
+  
+`
+
+const HeroSection = () => {
+  return (
+    <Container style={{backgroundImage: `url (${BG})`}}>
+    </Container>
+  )
+}
+
+export default HeroSection
