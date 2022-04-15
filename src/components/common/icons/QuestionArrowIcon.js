50255836a2921c1afc2452da0aa6ba7d2112c5f0
mjan123 <m.janbugti12@gmail.com>
2022-04-15 16:53:30 +0500

    Only FAQ img remaining

--- src/components/common/icons/QuestionArrowIcon.js
+++ src/components/common/icons/QuestionArrowIcon.js
@@ -3,47 +3,16 @@ import React from "react";
 const QuestionArrowIcon = () => {
   return (
     <svg
-      width="30"
-      height="32"
-      viewBox="0 0 30 32"
-      fill="none"
+      width="24"
+      height="26"
+      viewBox="0 0 24 26"
+      fill="#ffffff"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle
-        cx="15"
-        cy="15.7251"
-        r="14.75"
-        fill="black"
+      <path
+        d="M7.34774 1.51268C13.9562 2.56893 17.3285 2.49865 22.8694 1.42804M22.8694 1.42804C21.2745 7.35393 21.4188 10.6566 22.9505 16.2986M22.8694 1.42804L0.580202 25.5719"
         stroke="white"
-        stroke-width="0.5"
       />
-      <g clip-path="url(#clip0_246_881)">
-        <path
-          d="M15.3539 16.1215C15.1586 15.9263 14.8421 15.9263 14.6468 16.1215L10.4648 20.3035C10.2695 20.4988 10.2695 20.8154 10.4648 21.0106C10.6601 21.2059 10.9767 21.2059 11.1719 21.0106L15.0003 17.1822L18.8288 21.0106C19.024 21.2059 19.3406 21.2059 19.5359 21.0106C19.7311 20.8154 19.7311 20.4988 19.5359 20.3035L15.3539 16.1215ZM15.5003 31.4751V16.4751H14.5003V31.4751H15.5003Z"
-          fill="url(#paint0_linear_246_881)"
-        />
-      </g>
-      <defs>
-        <linearGradient
-          id="paint0_linear_246_881"
-          x1="15.0003"
-          y1="21.9751"
-          x2="15.0003"
-          y2="29.9751"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="white" stop-opacity="0" />
-        </linearGradient>
-        <clipPath id="clip0_246_881">
-          <rect
-            width="12"
-            height="16"
-            fill="white"
-            transform="translate(9 15.7251)"
-          />
-        </clipPath>
-      </defs>
     </svg>
   );
 };
